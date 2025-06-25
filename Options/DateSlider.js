0d20b14172a5b868aaaa8bbc0b519eeac8cfc164
Aayush Mishra <manofganges@gmail.com>
2025-06-25 11:56:16 +0530

    Update DateSlider.js

--- Options/DateSlider.js
+++ Options/DateSlider.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './DateSlider.css';
 
-
-const DateSlider = ({min, max, value, onChange, onRender}) => {
+const DateSlider = ({ min, max, value, onChange, onRender }) => {
     return (
-        <div className='DateSliderWrapper'>
-            {onRender ? <span className='Value'>{onRender(value)}</span> : null}
-            <input type="range" min={min} max={max} value={value} className="DateSlider" onChange={onChange} />
+        <div className="DateSliderWrapper">
+            {onRender && (
+                <span className="Value">
+                    {onRender(value)}
+                </span>
+            )}
+            <input
+                type="range"
+                min={min}
+                max={max}
+                value={value}
+                className="DateSlider"
+                onChange={onChange}
+            />
         </div>
-    )
-}
+    );
+};
 
 export default DateSlider;
